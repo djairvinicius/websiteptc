ba3c238367bbed45f28e4845beb0a8fd127a6165
Djair Vinicius <103010124+djairvinicius@users.noreply.github.com>
2026-01-31 15:39:21 -0300

    Adicionado arquivos HTML, CSS e JS para a estrutura inicial do projeto

---
+++ 260131/assets/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("titleone").innerHTML="PTC JS 260131";
+document.getElementById("textone").innerHTML="WebDeveloper 2026";
+document.getElementById("texttwo").innerHTML="© 2026 Djair Vinicius";
